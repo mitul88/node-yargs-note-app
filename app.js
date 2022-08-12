55f5afa34d3559eedaa8e77fd75faa1cd833891f
shahed <shahed.rahman88@gmail.com>
2022-08-12 14:23:08 +0600

    load notes, add notes, save notes added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require("./notes");
+const notes = require("./notes");
 
 
 yargs.version("1.1.0");
@@ -21,6 +21,7 @@ yargs.command({
         }
     },
     handler: function(argv) {
+        notes.addNote(argv.title, argv.body);
         console.log(chalk.bgGreen.bold("NOTE TITLE: " + argv.title));
         console.log(chalk.green.bold("NOTE BODY: " + argv.body));
     }
--- notes.js
+++ notes.js
@@ -1,9 +1,31 @@
+const fs = requre('fs');
+
 const getNotes = () => {
     return "Your notes ... "
 }
 
 const addNote = (title, body) => {
+    const notes = loadNotes();
+    notes.push({
+        title: title,
+        body: body
+    })
+    saveNotes(notes);
+}
 
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+const loadNotes = () => {
+    try{
+        const dataBuffer = fsreadFileSync('notes.json');
+        const dataJson = dataBuffer.toString();
+        return JSON.parse(dataJson);
+    } catch (e) {
+        return []
+    }
 }
 
 module.exports = {
